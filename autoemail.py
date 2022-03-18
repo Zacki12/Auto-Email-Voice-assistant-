6f17a477d00b78c598df3b62026b7c203f7324b6
Zack <67312419+Zacki12@users.noreply.github.com>
2022-03-18 08:51:52 -0700

    Add files via upload

---
+++ autoemail.py
@@ -0,0 +1,29 @@
+import speech_recognition as sr 
+import yagmail
+
+recognizer=sr.Recognizer()
+with sr.Microphone() as source:
+    print("Clearing Background noise..")
+    recognizer.adjust_for_ambient_noise(source,duration=1)
+    print("waiting for you message...")
+    recordedaudio=recognizer.listen(source)
+    print("done recording...!")
+try:
+     print('Printing the message..')
+     text=recognizer.recognize_google(recordedaudio,language='en-US')
+
+     print('your message:{}'.format(text))
+except Exception as ex:
+    print(ex)
+
+
+reciver='muzammil.idrees.bsse-2018b@cecosian.edu.pk' 
+message=text
+sender=yagmail.SMTP('muzammil.idrees12@gmail.com')
+sender.send(to=reciver,subject='This ia as automated mail', contents=message)   
+
+#Instagram: @zackkhan12
+# Github: @zacki12 
+
+
+
